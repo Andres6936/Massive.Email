c98e675056ed82d5f304bc4310d310896233609a
Andres6936 <andres6936@gmail.com>
2023-09-30 17:54:18 -0500

    Update Template

--- Template/Message.py
+++ Template/Message.py
@@ -10,13 +10,7 @@ def GetMessageTemplate(client: Sheet):
     return f"""
     <html lang='es'>
         <body>
-            <section>
-                <p>Bogot&aacute; D.C., 30 de Septiembre de 2023</p>
-        
-                <p>&nbsp;</p>
-                
-                <p>Se&ntilde;ores:</p>
-                
+            <section>                
                 <p>
                     {client.Name}<br />
                     NIT.: {client.Identification}<br />
@@ -26,55 +20,7 @@ def GetMessageTemplate(client: Sheet):
                     {client.Email}<br />
                     La ciudad<br />
                 </p>
-                
-                <p>&nbsp;</p>
-                
-                <p><strong>ASUNTO: CONTINUIDAD DE SERVICIOS PRESTADOS Y BIENVENIDA A RESIDUOS AMBIENTALES SAS</strong></p>
-                
-                <p>&nbsp;</p>
-                
-                <p>Reciba un Cordial Saludo,</p>
-                
-                <p>&nbsp;</p>
-                
-                <p>En atenci&oacute;n a nuestra comunicaci&oacute;n previa y su voluntad de continuidad con los servicios 
-                prestados a Usted(es) antes con AMBIENTALES NACIONALES SAS y ahora con <strong>RESIDUOS AMBIENTALES SAS</strong>;
-                le da la bienvenida a nuestra compa&ntilde;&iacute;a, as&iacute; mismo, se adjuntan los siguientes documentos:</p>
-                
-                <p>&nbsp;</p>
-                
-                <ol>
-                    <li>Contrato de Transporte Integral de Residuos &ndash; <strong>POR FAVOR DEVOLVER FIRMADO</strong></li>
-                    <li>Protocolo de Recolecci&oacute;n &ndash; Tips importantes para el GENERADOR.</li>
-                    <li>RAD de Plan de Contingencia SECRETARIA DE AMBIENTE Y CAR</li>
-                    <li>Licencia Ambiental Aliados (Prosarc y Ecoentorno)</li>
-                    <li>Brochure de Servicios RE-AM</li>
-                    <li>Certificaciones Bancarias y MEDIOS DE PAGO AUTORIZADOS: Nequi y Daviplata.</li>
-                    <li>C&aacute;mara de Comercio menor a 30 d&iacute;as</li>
-                    <li>Rut</li>
-                    <li>Fotocopia de CC RL</li>
-                </ol>
-                
-                <p>&nbsp;</p>
-                
-                <p>Agradecemos inmensamente su decisi&oacute;n, voluntad y gran confianza depositada, bajo nuestra inquebrantable voluntad y disposici&oacute;n de prestar mejores servicios esenciales para desarrollar a plenitud el objeto social de nuestros aliados.</p>
-                
-                <p>&nbsp;</p>
             </section>
-            
-            
-             <footer>
-                 <p>Atentamente, <br /></p>
-                 <p style="font-size: 1.3rem"><strong>ORIGINAL FIRMADO</strong> <br /></p>
-            
-                 <p>
-                     <strong>YOMAIRA BEYTAR FLOREZ</strong><br />
-                     <strong>C.C. 39.305.329</strong><br />
-                     <strong>REPRESENTANTE LEGAL</strong><br />
-                     <strong>RESIDUOS AMBIENTALES SAS</strong><br />
-                     <strong>NIT.: 901.747.145-4</strong>
-                </p>
-             </footer>
         </body>
     </html>
 """
