9c6244f249fb0433b7ad16cc43564ce1ce5f6013
Andres6936 <andres6936@gmail.com>
2024-04-13 19:24:09 -0500

    Update PDF file handling in splitter.js
    Enhanced the PDF file handling function in splitter.js to support the processing of multiple files. A new asynchronous generator function was introduced for reading directories and files. This change allows the code to dynamically read, process, and create a directory for each individual PDF file, significantly enhancing flexibility and efficiency.

--- Certificate/node/splitter.js
+++ Certificate/node/splitter.js
@@ -2,16 +2,43 @@
 
 import muhammara from 'muhammara'
 import fs from 'node:fs'
+import {readdir} from 'node:fs/promises'
+import {resolve, basename} from "node:path";
 
-const reader = muhammara.createReader("../obj/N22.pdf")
-const pages = reader.getPagesCount();
-
-fs.mkdirSync("../obj/N22", {
-    recursive: true
-})
-
-for (let i = 1; i <= pages; i++) {
-    const writer = muhammara.createWriter("../obj/N22/Output"  +  i + ".pdf")
-    writer.createPDFCopyingContext(reader).appendPDFPageFromPDF(i - 1);
-    writer.end();
+// Ref: https://stackoverflow.com/a/45130990
+async function* getFiles(dir) {
+    const dirents = await readdir(dir, {withFileTypes: true});
+    for (const dirent of dirents) {
+        const res = resolve(dir, dirent.name);
+        if (dirent.isDirectory()) {
+            yield* getFiles(res);
+        } else {
+            yield res;
+        }
+    }
 }
+
+
+(async () => {
+    for await (const file of getFiles('pdf/')) {
+        console.log('Processing the template file %s', file)
+
+        const basenameOfFile = basename(file).replace('.pdf', '')
+        const objDumpOfSeparatedFiles = `obj/${basenameOfFile}`;
+
+        const reader = muhammara.createReader(file)
+        const pages = reader.getPagesCount();
+
+        fs.mkdirSync(objDumpOfSeparatedFiles, {
+            recursive: true
+        })
+
+        for (let i = 1; i <= pages; i++) {
+            console.log('Separated page %s', i)
+            const writer = muhammara.createWriter(`${objDumpOfSeparatedFiles}/Output${i}.pdf`)
+            writer.createPDFCopyingContext(reader).appendPDFPageFromPDF(i - 1);
+            writer.end();
+        }
+    }
+})()
+
